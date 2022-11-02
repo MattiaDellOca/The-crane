877a54e5e728511b59eabd760d7b482c10a19bf9
Manuele Nolli <manuele.nolli@student.supsi.ch>
2022-11-02 21:13:45 +0100

    started milestone serie3

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,32 @@
 #include "main.h"
+
+void myKeyboardCallback(unsigned char key, int x, int y) {
+
+   if (key == ' ') {
+      cout << "spacebar pressed" << endl;
+   }
+}
+
+
 int main(int argc, char* argv[]) {
    cout << "Hello world!" << endl;
-   DynLib::init();
-   DynLib::doNothing();
-   DynLib::free();
+   MyEngine::init("My OpenGL window", 640, 480);
+   MyEngine::setKeyboardCallback(myKeyboardCallback);
+   MyEngine::setBackgroundColor(0.2f, 0.2f, 0.7f);
+   //while (MyEngine::isRunning()) {
+   while (true) {
+
+   
+      MyEngine::clearWindow();
+      
+      //draw Something
+      MyEngine::doSomething();
+      
+      MyEngine::swapBuffers(); 
+   }
+      //} 
+   MyEngine::free();
+   return 0;
+
 
 }
