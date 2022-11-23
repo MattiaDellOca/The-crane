b52322143fdb488d6562f9084fe397369f3b19b0
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-11-23 18:26:10 +0100

    Added run method + added command line arguments

--- graphicsEngine/Engine.h
+++ graphicsEngine/Engine.h
@@ -26,6 +26,7 @@
 /////////////
 #include "node.h"
 #include "camera.h"
+#include <functional>
 
 /**
  * @brief Simple static class example.
@@ -34,23 +35,29 @@ class LIB_API Engine {
 private:
 	Node* m_scene_graph;
 
-	Engine() {};
-	~Engine() {}
+	Engine() : m_scene_graph{nullptr} {};
+	~Engine() {
+		delete m_scene_graph;
+	}
 	static bool m_initFlag;
 	static Engine* m_instance;
 	static bool m_isRunning;
 	static int m_windowId;
+	static void reshapeCallback(int, int);
+	static void displayCallback();
 public:
 	static Engine* getInstance();
-	static bool init(const char*, unsigned int, unsigned int);
+	static bool init(const char*, unsigned int, unsigned int, int*, char**);
 	static Node* load(std::string);
 	static void clear();
 	static void setBackgroundColor(float, float, float);
-	static void begin3D(Camera);
+	static void begin3D(Camera*);
 	static void end3D();
 	static void swapBuffers();
 	static bool free();
 	static void render();
+
+	static void run(void (*)());
 
 
 	// Callback setters
