a526fcb25cd3b31825df6ad571739bbb89e40202
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-11-23 18:24:49 +0100

    Added some testing methods + added imports

--- client/main.cpp
+++ client/main.cpp
@@ -9,23 +9,23 @@ void keyboardCallback(unsigned char key, int x, int y) {
 
 int main(int argc, char* argv[]) {
    cout << "Hello world!" << endl;
-   Engine::init("My OpenGL window", 650, 650);
+   Engine::init("My OpenGL window", 650, 650, &argc, argv);
+   cout << "ENDED CONFIG" << endl;
    Engine::setKeyboardFunction(keyboardCallback);
    Engine::setBackgroundColor(0.2f, 0.2f, 0.7f);
 
-   while (Engine::isRunning()) {
-       while (true) {
-          // Clear image
-          Engine::clear();
+   // Start rendering some figures..
+   Engine::run([](void) {
+       // Clear image
+       Engine::clear();
 
-          // Force engine to re-render everything!
-          Engine::render();
-        
-          // Swap buffers!
-          Engine::swapBuffers(); 
-       }
-   } 
+       // Force engine to re-render entire scene!
+       Engine::render();
 
+       // Swap buffers to show rendered image
+       Engine::swapBuffers();
+   });
+
+   // Free engine
    Engine::free();
-   return 0;
 }
--- client/main.h
+++ client/main.h
@@ -1,4 +1,7 @@
 #include <iostream>
 #include "engine.h"
+#include "camera.h"
+#include "orthographicCamera.h"
+#include "perspectiveCamera.h"
 
 using namespace std; 
