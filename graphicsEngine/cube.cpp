f8be40658b1e2309d5ce9858694e20d298ba2173
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-12-12 17:35:43 +0100

    Now class mesh::render first

--- graphicsEngine/cube.cpp
+++ graphicsEngine/cube.cpp
@@ -5,9 +5,12 @@
 
 LIB_API Cube::Cube(float edge, std::string name, glm::mat4 matrix, Material *material) : Mesh{ name,matrix, material }, m_edge{edge} {};
 
-void LIB_API Cube::render(glm::mat4) {
+void LIB_API Cube::render(glm::mat4 coords) {
 	// Render cube
 	std::cout << "CUBE: edge: " << m_edge << " Name: " << m_name << std::endl;
+
+	// Load superclass
+	Mesh::render(coords);
 
 	// Show cube
 	glutSolidCube(m_edge);
--- graphicsEngine/sphere.cpp
+++ graphicsEngine/sphere.cpp
@@ -5,9 +5,14 @@
 
 LIB_API Sphere::Sphere(float radius, std::string name, glm::mat4 matrix, Material *material) : Mesh(name, matrix, material), m_radius{ radius } {};
 
-void LIB_API Sphere::render(glm::mat4) {
+void LIB_API Sphere::render(glm::mat4 coords) {
 
 	// Render sphere
 	std::cout << "SPHERE: radius: " << m_radius << " Name: " << m_name << std::endl;
+
+	// Load superclass
+	Mesh::render(coords);
+
+	// Draw sphere
 	glutSolidSphere(m_radius, (int)8 * 5, (int)8 * 5);
 }
