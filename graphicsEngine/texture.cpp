7906b45a0b7fa9f432d81ebe10895fac24b1f2c0
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-12-17 12:37:09 +0100

    Added freeimage

--- graphicsEngine/texture.cpp
+++ graphicsEngine/texture.cpp
@@ -6,16 +6,35 @@
 	//GLM:
 #include <glm/gtc/type_ptr.hpp>
 
-   //FreeGLUT:
+	//FreeGLUT:
 #include <GL/freeglut.h>
 
+	//FreeImage:
+#include <FreeImage.h>
 
-LIB_API Texture::Texture(std::string name, std::string path) : Object{name} {
+bool Texture::m_is_init = false;
+
+LIB_API Texture::Texture(std::string name, std::string path) : Object{ name } {
 	// Load texture from filepath
 	glGenTextures(1, &m_texId);
+	glBindTexture(GL_TEXTURE_2D, m_texId);
 
 	// Load bitmap data from file path
+	if (!m_is_init) {
+		// init freeimage
+		FreeImage_Initialise();
+		m_is_init = true;
+	}
 
+	// Find filename
+	std::string tex_filename = path.substr(path.find_last_of("/\\") + 1);
+	
+	// Load image
+	m_data = FreeImage_Load(FreeImage_GetFileType(path.c_str(), 0), tex_filename.c_str());
+
+	// Load height + width
+	m_height = FreeImage_GetHeight(static_cast<FIBITMAP*>(m_data));
+	m_width = FreeImage_GetHeight(static_cast<FIBITMAP*>(m_data));
 };
 
 LIB_API Texture::Texture(std::string name, unsigned int width, unsigned int height, void* bitmap) : Object{ name }, m_width{ width }, m_height{ height }, m_data { bitmap } {
--- graphicsEngine/texture.h
+++ graphicsEngine/texture.h
@@ -4,15 +4,11 @@
 
 class LIB_API Texture : public Object {
 private:
+	static bool m_is_init;
 	unsigned int m_texId;
 	unsigned int m_height;
 	unsigned int m_width;
 	void* m_data;
-
-
-	bool isFileExisting();
-	bool isFileAccessible();
-	void* readFile();
 public:
 	Texture(std::string name, std::string path);
 	Texture(std::string name, unsigned int width, unsigned int height, void* bitmap);
