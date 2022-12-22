df7b88dab17d6fe160f5c3fe5e1b04441faeffa2
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-12-22 13:15:41 +0100

    Added comments

--- engine/src/directionalLight.h
+++ engine/src/directionalLight.h
@@ -2,10 +2,26 @@
 
 #include "light.h"
 
+/**
+ * DirectionalLight class that describes a directional light (aka infinite light).
+ */
 class LIB_API DirectionalLight : public Light {
-
 public:
-	DirectionalLight(std::string, glm::mat4, glm::vec4, glm::vec4, glm::vec4);
+	/**
+	 * Constructor method.
+	 * 
+	 * \param name Node name
+	 * \param matrix Transformation matrix
+	 * \param ambient Ambient term vector
+	 * \param diffuse Diffuse term vector
+	 * \param specular Specular term vector
+	 */
+	DirectionalLight(std::string name, glm::mat4 matrix, glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular);
 
-	virtual void render(glm::mat4);
+	/**
+	 * Render the light in the scene.
+	 * 
+	 * \param coords pre-computed world coordinates
+	 */
+	virtual void render(glm::mat4 coords);
 };
