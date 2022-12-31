102059ec712d0e4ef3d1753e2850f92d85fcd64f
Luca Di Bello <luca.dibello@student.supsi.ch>
2022-12-31 11:11:24 +0100

    HOTFIX: Fixed code style

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -19,7 +19,6 @@ Node* container2;
 const float heightContainer = 181.391907f;
 Node* plane;
 
-
 // Settings
 float cameraSpeed = 100.0f;
 float craneRotationSpeed = 1.f;
@@ -33,7 +32,7 @@ Node* hookedNode = nullptr;
 float extensionsCableCount = 0;
 float movementTrolleyCount = 0;
 
-//Range of movement
+// Range of movement
 const float minTrolley = -50;
 const float maxTrolley = 6;
 const float maxExtensionsCable = 50;
@@ -89,12 +88,12 @@ void keyboardCallback(unsigned char key, int x, int y) {
 		slewingUnit->setMatrix(glm::rotate(slewingUnit->getMatrix(), glm::radians(craneRotationSpeed), glm::vec3(0.0f, 1.f, 0.f)));
 	}
 	else if (key == 'l') {
-		//rotate crane counter clockwise
+		// rotate crane counter clockwise
 		cout << "[CRANE] SleewingUnit: rotating counterclockwise" << endl;
 		slewingUnit->setMatrix(glm::rotate(slewingUnit->getMatrix(), glm::radians(-craneRotationSpeed), glm::vec3(0.0f, 1.f, 0.f)));
 	}
 	else if (key == 'h') {
-		//move trolley backward
+		// move trolley backward
 
 		if (minTrolley < movementTrolleyCount - 1) {
 			cout << "[CRANE] Trolley: moving backward" << endl;
@@ -107,8 +106,7 @@ void keyboardCallback(unsigned char key, int x, int y) {
 		}
 	}
 	else if (key == 'k') {
-		//move trolley forward
-
+		// move trolley forward
 		if (maxTrolley > movementTrolleyCount + 1) {
 			cout << "[CRANE] Trolley: moving forward" << endl;
 			trolley->setMatrix(glm::translate(trolley->getMatrix(), glm::vec3(trolleySpeed, 0.0f, 0.0f)));
@@ -120,18 +118,18 @@ void keyboardCallback(unsigned char key, int x, int y) {
 		}
 	}
 	else if (key == 'u') {
-		//move cable up
+		// move cable up
 
 		if (extensionsCableCount > 0) {
 			cout << "[CRANE] Cables: moving up" << endl;
-
-			//There is an object between the hook and the cable
+ 
+			// There is an object between the hook and the cable
 			Node* hookParent = cable1->getChildren().at(0);
 
 			cable1->setMatrix(glm::scale(cable1->getMatrix(), glm::vec3(1.0f, 1.0f - cablesScaleSpeed, 1.0f)));
 			cable2->setMatrix(glm::scale(cable2->getMatrix(), glm::vec3(1.0f, 1.0f - cablesScaleSpeed, 1.0f)));
 
-			//adapt matrix
+			// adapt matrix
 			hookParent->setMatrix(glm::scale(hookParent->getMatrix(), glm::vec3(1.0f, 1.0f + cablesScaleSpeed, 1.0f)));
 
 			extensionsCableCount--;
@@ -139,22 +137,20 @@ void keyboardCallback(unsigned char key, int x, int y) {
 		else {
 			cout << "[CRANE] Cables: can't moving up" << endl;
 		}
-
-
 	}
 	else if (key == 'j') {
-		//move cable down
+		// move cable down
 
 		if (maxExtensionsCable > extensionsCableCount + 1) {
 			cout << "[CRANE] Cables: moving down" << endl;
 
-			//There is an object between the hook and the cable
+			// There is an object between the hook and the cable
 			Node* hookParent = cable1->getChildren().at(0);
 
 			cable1->setMatrix(glm::scale(cable1->getMatrix(), glm::vec3(1.0f, 1.0f + cablesScaleSpeed, 1.0f)));
 			cable2->setMatrix(glm::scale(cable2->getMatrix(), glm::vec3(1.0f, 1.0f + cablesScaleSpeed, 1.0f)));
 
-			//adapt matrix
+			// adapt matrix
 			hookParent->setMatrix(glm::scale(hookParent->getMatrix(), glm::vec3(1.0f, 1.0f - cablesScaleSpeed, 1.0f)));
 
 			extensionsCableCount++;
@@ -162,103 +158,85 @@ void keyboardCallback(unsigned char key, int x, int y) {
 		else {
 			cout << "[CRANE] Cables: can't moving down" << endl;
 		}
-
-
 	}
 	else if (key == 'm' && !hooked) {
-		//hook one of the containers
+		// hook one of the containers
 
-		//Get world coordinates
+		// Get world coordinates
 		glm::vec3 worldCordhook{ hook->getWorldCoordinateMatrix()[3] };
 		glm::vec3 worldCordContainer1{ container1->getWorldCoordinateMatrix()[3] };
 		glm::vec3 worldCordContainer2{ container2->getWorldCoordinateMatrix()[3] };
 
-		//Calculate distance
+		// Calculate distance
 		float distanceHookContainer1 = glm::distance(worldCordhook, worldCordContainer1);
 		float distanceHookContainer2 = glm::distance(worldCordhook, worldCordContainer2);
 
-		//Hook the object if is it near
+		// Hook the object if is it near
 		if (distanceHookContainer1 < thresholdHook) {
 			cout << "[CRANE] Hook: hooking container1" << endl;
 
-			//Calculate matrix of the container hooked
+			// Calculate matrix of the container hooked
 			glm::mat4 matrixContainer1 = glm::inverse(hook->getWorldCoordinateMatrix()) * container1->getWorldCoordinateMatrix();
-			//reset world coordinates
+			// Reset world coordinates
 			matrixContainer1[3] = glm::vec4(0.f, 0.f, 0.f, 1.f);
-
-			//Change the scene
+			// Change the scene
 			container1->getParent()->removeChild(container1);
 			container1->setParent(hook);
 			container1->setMatrix(matrixContainer1);
-
-			//Set hooked as true
+			// Set hooked as true
 			hooked = !hooked;
 			hookedNode = container1;
 		}
 		else if (distanceHookContainer2 < thresholdHook) {
 			cout << "[CRANE] Hook: hooking container2" << endl;
 
-			//Calculate matrix of the container hooked
+			// Calculate matrix of the container hooked
 			glm::mat4 matrixContainer2 = glm::inverse(hook->getWorldCoordinateMatrix()) * container2->getWorldCoordinateMatrix();
-			//reset world coordinates
+			// Reset world coordinates
 			matrixContainer2[3] = glm::vec4(0.f, 0.f, 0.f, 1.f);
-
-			//Change the scene
+			// Change the scene
 			container2->getParent()->removeChild(container2);
 			container2->setParent(hook);
 			container2->setMatrix(matrixContainer2);
-
-			//Set hooked as true
+			// Set hooked as true
 			hooked = !hooked;
 			hookedNode = container2;
-
 		}
 		else {
 			cout << "[CRANE] Hook: The containers are too far apart" << endl;
 		}
-
 	}
 	else if (key == 'm' && hooked) {
-
-		//Get world coordinates
+		// Get world coordinates
 		glm::vec3 worldObjHooked{ hookedNode->getWorldCoordinateMatrix()[3] };
 		glm::vec3 worldCordPlane{ plane->getWorldCoordinateMatrix()[3] };
-
-		//Calculate distance
+		// Calculate distance
 		float distanceObjPlane = worldObjHooked.y - worldCordPlane.y - heightContainer;
-
 		cout << distanceObjPlane << endl;
 
 		if (distanceObjPlane < abs(thresholdHook)) {
-			//Release the container
+			// Release the container
 			cout << "[CRANE] Hook: release object" << endl;
-
-			//world coordinates hooked obj
+			// World coordinates hooked obj
 			glm::mat4 worldCoordHooked = hookedNode->getWorldCoordinateMatrix();
 			worldCoordHooked[3].y = heightContainer;
-
-			//Change the scene
+			// Change the scene
 			hookedNode->getParent()->removeChild(hookedNode);
 			hookedNode->setParent(root);
 			hookedNode->setMatrix(worldCoordHooked);
-
-			//Set hooked as true
+			// Set hooked as true
 			hooked = !hooked;
 			hookedNode = nullptr;
-
 		}
 		else {
 			cout << "[CRANE] Hook: the container is too far from the plane" << endl;
 		}
 		//hooked = !hooked;
-
 	}
 	else {
 		cout << key << " not supported" << endl;
 	}
 }
-
-
 
 int main(int argc, char* argv[]) {
 	cout << "[Crane - SUPSI]" << endl;
@@ -279,7 +257,7 @@ int main(int argc, char* argv[]) {
 	Engine::setGraphics(profile);
 	Engine::load("..\\assets\\crane\\crane.ovo", "..\\assets\\crane");
 
-	//Searching nodes
+	// Searching nodes
 	root = Engine::getNode("[root]");
 	crane = Engine::getNode("Crane");
 	slewingUnit = Engine::getNode("SlewingUnit");
@@ -296,5 +274,4 @@ int main(int argc, char* argv[]) {
 
 	// Free engine
 	Engine::free();
-
 }	
