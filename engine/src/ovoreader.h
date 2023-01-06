d9a17cb1c972f70053f0f098c6c8630c0ba590bf
Manuele Nolli <manuele.nolli@student.supsi.ch>
2023-01-06 17:07:05 +0100

    HOTFIX: removed textureDir default path

--- engine/src/ovoreader.h
+++ engine/src/ovoreader.h
@@ -162,10 +162,10 @@ public:
      * \brief Parse a 3D Ovo model into a scene graph.
      * 
      * \param path Path of the Ovo 3D model file.
-     * \param texturesDirectory (Optional) Path where textures are stored, default '.\\assets'.
+     * \param texturesDirectory (Optional) Path where textures are stored.
      * \return Scene graph that describes the read 3D model.
      */
-    Node* readFile(const char* path, const char* texturesDir="..\\assets");
+    Node* readFile(const char* path, const char* texturesDir);
 protected:
 
     /**
