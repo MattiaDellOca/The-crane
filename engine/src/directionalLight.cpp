f5bc8e5d273cfd0e9e78b2287caf68ad578001ec
Manuele Nolli <manuele.nolli@student.supsi.ch>
2023-03-26 20:50:48 +0200

    removed comment

--- engine/src/directionalLight.cpp
+++ engine/src/directionalLight.cpp
@@ -15,39 +15,9 @@ LIB_API DirectionalLight::DirectionalLight(const std::string& name, glm::mat4 ma
 
 LIB_API void DirectionalLight::render(glm::mat4 matrix)
 {
-	//Position in (0,0,0) and the translation matrix will move the light
-	//Light position is set to object coordinates and is modified by the current OpenGL matrix (as with any other object):
-
-	//Values for omnidirectional light
-	//glm::vec4 objectCoordPosition(0.f, 0.f, 0.f, 0.f); //w = 0
-
-
 	Shader* progShader = ShaderManager::GetShader("programShaderDirectionalLight");
 	progShader->setVec3(progShader->getParamLocation("lightDirection"), m_direction);
 	progShader->setVec3(progShader->getParamLocation("lightAmbient"), m_lightAmbient);
 	progShader->setVec3(progShader->getParamLocation("lightDiffuse"), m_lightDiffuse);
 	progShader->setVec3(progShader->getParamLocation("lightSpecular"), m_lightSpecular);
-	
-	
-
-
-
-	//Reset attenuation
-
-	/*
-	glLightf(m_baseValueLights + m_lightId, GL_CONSTANT_ATTENUATION, 1.f);
-	glLightf(m_baseValueLights + m_lightId, GL_LINEAR_ATTENUATION, 0.f);
-	glLightf(m_baseValueLights + m_lightId, GL_QUADRATIC_ATTENUATION, 0.f);
-
-
-	//Load matrix
-	glLoadMatrixf(glm::value_ptr(matrix));
-
-	//Settings
-	glLightfv(m_baseValueLights + m_lightId, GL_POSITION, glm::value_ptr(objectCoordPosition));
-
-	glLightfv(m_baseValueLights + m_lightId, GL_AMBIENT, glm::value_ptr(m_lightAmbient));
-	glLightfv(m_baseValueLights + m_lightId, GL_DIFFUSE, glm::value_ptr(m_lightDiffuse));
-	glLightfv(m_baseValueLights + m_lightId, GL_SPECULAR, glm::value_ptr(m_lightSpecular));
-	*/
 }
