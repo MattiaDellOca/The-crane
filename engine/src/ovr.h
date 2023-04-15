12d9f8b5f54cf96cabcffbf8c9d97a972b8b2cc4
Manuele Nolli <manuele.nolli@student.supsi.ch>
2023-04-15 18:04:00 +0200

    Separated ovr in .cpp and .h

--- engine/src/ovr.h
+++ engine/src/ovr.h
@@ -1,1 +1,68 @@
+/**
+ * @file		ovr.h
+ * @brief	Self-contained helper class for interfacing OpenGL and OpenVR. Shortened version of Overvision's OvVR module.
+ *
+ * @author	Achille Peternier (C) SUPSI [achille.peternier@supsi.ch]
+ */
 #pragma once
+#include "lib.h"
+
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace vr
+{
+	class IVRSystem;
+	class IVRRenderModels;
+	class IVRCompositor;
+	struct TrackedDevicePose_t;
+	struct HmdMatrix34_t;
+	struct HmdMatrix44_t;
+}
+
+struct Controller;
+
+class LIB_API OvVR
+{
+public:
+	enum OvEye
+	{
+		EYE_LEFT = 0,
+		EYE_RIGHT = 1,
+		EYE_LAST
+	};
+
+	OvVR();
+	~OvVR();
+
+	bool init();
+	bool free();
+	std::string getTrackingSysName();
+	bool printRenderModels();
+	std::string getManufacturerName();
+	std::string getModelNumber();
+	unsigned int getHmdIdealHorizRes();
+	unsigned int getHmdIdealVertRes();
+	static glm::mat4 ovr2ogl(const vr::HmdMatrix34_t& matrix);
+	static glm::mat4 ovr2ogl(const vr::HmdMatrix44_t& matrix);
+	bool update();
+	glm::mat4 getProjMatrix(OvEye eye, float nearPlane, float farPlane);
+	glm::mat4 getEye2HeadMatrix(OvEye eye);
+	glm::mat4 getModelviewMatrix();
+	unsigned int getNrOfControllers();
+	Controller* getController(unsigned int pos) const;
+	void setReprojection(bool flag);
+	void pass(OvEye eye, unsigned int eyeTexture);
+	void render();
+
+
+private:
+	// OVR objects:	
+	vr::IVRSystem* vrSys;
+	vr::IVRRenderModels* vrModels;
+	vr::IVRCompositor* vrComp;
+	std::vector < vr::TrackedDevicePose_t> vrPoses;
+	std::vector<Controller*> controllers;
+};
+
