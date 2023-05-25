1dca0f95a5e21f2bd36222c6d01d56646f789b4a
Manuele Nolli <manuele.nolli@student.supsi.ch>
2023-05-25 15:06:45 +0200

    Revert "Updated main.h, added class diagram"
    This reverts commit 3b4efc4745fbf941d8ba99bca8f7be207b2cf992.

--- client/src/main.h
+++ client/src/main.h
@@ -2,7 +2,12 @@
 #include "src/engine.h"
 #include "src/camera.h"
 #include "src/node.h"
+//#include "src/orthographicCamera.h"
 #include "src/perspectiveCamera.h"
+//#include "src/light.h"
+//#include "src/directionalLight.h"
+//#include "src/omnidirectionalLight.h"
+//#include "src/spotLight.h"
 
 #include "cameraNode.h"
 #include "configReader.h"
